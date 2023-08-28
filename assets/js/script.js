c3e87a5e1a4f08e82c826e760cc789d04e0b3916
arthurborges <arthurborges.senai564@gmail.com>
2023-08-28 08:22:18 -0300

    feat: sendErrorMsg function fix

--- assets/js/script.js
+++ assets/js/script.js
@@ -25,10 +25,39 @@ class PetsList{
         this.pets = [];
     }
     addPet(pet){
-        this.pets.push(pet);
+        if(isAnyInputEmpty()){
+            sendErrorMsg("Preencha todos os campos!", "error");
+        } else{
+            this.pets.push(pet);
+            sendErrorMsg("Pet cadastrado com sucesso!", "success");
+        }
     }
 }
-function registerPet(){
+function sendErrorMsg(msg, type){
+    let errorMsg = document.getElementById("errorMsg");
+    errorMsg.innerHTML = msg;
+    if(type == "error"){
+        errorMsg.style.color = "#ff0000";
+    }else if(type == "success"){
+        errorMsg.style.color = "#00ff00";
+    }
+}
+function isAnyInputEmpty(){
+    let owner = document.getElementById("owner").value;
+    let petname = document.getElementById("pet").value;
+    let specie = document.getElementById("specie").value;
+    let photo = document.getElementById("photo").value;
+    let birthdate = document.getElementById("birth").value;
+
+    if(owner == "" || petname == "" || specie == "" || photo == "" || birthdate == ""){
+        return true;
+    }else{
+        return false;
+    }
+
+}
+const ListOfPet = new PetsList();
+function regiserPet(){
     let owner = document.getElementById("owner").value;
     let petname = document.getElementById("pet").value;
     let specie = document.getElementById("specie").value;
@@ -36,14 +65,11 @@ function registerPet(){
     let birthdate = document.getElementById("birth").value;
 
     let pet = new Pet(owner, petname, specie, photo, birthdate);
-    ListOfPet.add(pet);
+    ListOfPet.addPet(pet);
 }
 function dateinPTBR(date) {
-    console.log("Passou pela funcao dateinPTBR()");
-
     let dateArray = date.split("-");
     let datePTBR = dateArray[2] + "/" + dateArray[1] + "/" + dateArray[0];
     return datePTBR;
 }
-const ListOfPet = new PetsList();
 console.log(ListOfPet);
